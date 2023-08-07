990252e29bdcc0846b478bcab888952517cdcfcb
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2023-08-07 18:36:38 +0200

    restructure code

--- skcan_compare/__init__.py
+++ skcan_compare/__init__.py
@@ -22,46 +22,42 @@ class SkcanCompare(object):
     Base class for accessing functionality
     """
 
-    def __init__(self, endpoint=globals.BLAZEGRAPH_ENDPOINT):
+    def __init__(self, species="Homo sapiens", endpoint=globals.BLAZEGRAPH_ENDPOINT):
         self.endpoint = endpoint
         self.region_dict = {}
         self.vis = None
-        self.species = None
+        self.species = species
+        self.load_json_file()
 
     def execute_query(self, query_string):
         # execute specified SPAQRL query and return result
         return query.sparql_query(query_string, endpoint=self.endpoint)
 
 
-    def load_json_file(self, species="human"):
-        if species == "human":
-            datapath = pkg_resources.resource_filename("skcan_compare", "data")
-            filepath = os.path.join(datapath, "coords_human.json")
+    def load_json_file(self):
+        datapath = pkg_resources.resource_filename("skcan_compare", "data")
+        if self.species == "Mus musculus":
+            filepath = os.path.join(datapath, "coords_mouse.json")
+        elif self.species == "Rattus norvegicus":
+            filepath = os.path.join(datapath, "coords_rat.json")
         else:
-            # todo: add other species
-            return
+            # default
+            filepath = os.path.join(datapath, "coords_human.json")
 
         with open(filepath, encoding='utf-8-sig') as json_file:
             data = json.load(json_file)
 
-        self.region_dict[species] = {}
+        self.region_dict[self.species] = {}
         for item in data:
-            self.region_dict[species][item["Name"]] = [int(item["X"]), int(item["Y"])]
-
-    def init_vis(self, species=None):
-        if species not in self.region_dict.keys():
-            self.load_json_file(species=species)
-        if not self.vis or self.species!=species:
-            self.vis = Visualizer(self.region_dict[species], species)
-            self.species = species
-        return self.vis
-    
+            self.region_dict[self.species][item["Name"]] = [int(item["X"]), int(item["Y"])]
+   
     def reset_vis(self):
-        self.vis = Visualizer(self.region_dict[self.species])
+        self.vis = Visualizer(self.region_dict[self.species], self.species)
         return self.vis
 
-    def add_connection(self, species=None, region_A=None, region_B=None, region_C=None, neuron=None):
-        vis = self.init_vis(species)
+    def add_connection(self, region_A=None, region_B=None, region_C=None, neuron=None):
+        if not self.vis:
+            self.vis = Visualizer(self.region_dict[self.species], self.species)
 
         if not region_A:
             raise ValueError("region_A needs to be specified!")
@@ -69,13 +65,15 @@ class SkcanCompare(object):
             raise ValueError("region_B needs to be specified!")
 
         if region_C:
-            # A->C->C
-            vis.draw_edge_ABC(region_A, region_B, region_C, neuron)
+            # A->C->B
+            self.vis.draw_edge_ABC(region_A, region_B, region_C, neuron)
         else:
             # A->B
-            vis.draw_edge_AB(region_A, region_B, neuron)
+            self.vis.draw_edge_AB(region_A, region_B, neuron)
 
-        return vis.get_figure()
+
+    def get_graph(self):
+        return self.vis.get_figure()
     
 
     def get_unique_region_IRI(self, df_result):
