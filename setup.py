3eec5e150ff4e882cee4fc2fce215094214edbfd
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2023-08-08 05:11:40 +0200

    add docs

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="sckan-compare",
-    version="0.0.6",
+    version="0.0.7",
     description="A package for retrieving and visualizing data contained in     SCKAN (e.g., across species, relationship to spinal segments) to highlight similarities and differences in neuronal pathways",
     author="Shailesh Appukuttan, Hiba Ben Aribi, Pranjal Garg, Gautam Kumar",
     author_email="appukuttan.shailesh@gmail.com",
     license="Apache-2.0",
     package_data={'': ['data/*.*']},
     packages=find_packages(),
+    install_requires=["dash", "dash-cytoscape", "numpy", "pandas", "plotly"],
 )
