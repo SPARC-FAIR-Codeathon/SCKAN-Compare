505fa4b5135407c9be728a52b9bd41f69828805a
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2023-08-22 15:27:41 +0200

    Update utils
    Add some generic functions to utils
    
    Changes needed: some functions use hard-coded values. Should be updated to be used with query outputs.

--- sckan_compare/utils.py
+++ sckan_compare/utils.py
@@ -1,3 +1,30 @@
+import pandas as pd
+
+def get_dataframe(data_as_list):
+    # convert data_as_list to pandas dataframe
+    df = pd.DataFrame(data_as_list)
+    #set column names equal to values in row index position 0
+    df.columns = df.iloc[0]
+    #remove first row from DataFrame
+    df = df[1:]
+    return df
+
+
+def remove_duplicate_species(df):
+    df = df.replace('Mammalia', 'Mammal')
+    df = df.replace('mammals', 'Mammal')
+    df = df.replace('Vertebrata <vertebrates>', 'Vertebrata')
+    df = df.replace('vertebrates', 'Vertebrata')
+    df = df.replace('human', 'Homo sapiens')
+    df = df.replace('Norway rat', 'Rattus norvegicus')
+    df = df.replace('brown rat', 'Rattus norvegicus')
+    df = df.replace('rat', 'Rattus norvegicus')
+    df = df.replace('rats', 'Rattus norvegicus')
+    df = df.replace('mouse', 'Mus musculus')
+    df = df.replace('house mouse', 'Mus musculus')
+    return df
+
+
 def get_species_name(projection_fibre=False):
     """
     :return: gets the (unique) name of all species in the dataset
