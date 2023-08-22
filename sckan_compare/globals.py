c3625c06f64a46b3685dab2f5f8b6f64a885075e
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2023-08-22 19:04:43 +0200

    Update globals.py
    Add dict mapping for species to their visual coordinate maps

--- sckan_compare/globals.py
+++ sckan_compare/globals.py
@@ -1,3 +1,9 @@
 BLAZEGRAPH_ENDPOINT = "https://blazegraph.scicrunch.io/blazegraph/sparql"
 
 DEFAULT_MAX_CACHE_DAYS = 7
+
+AVAILABLE_SPECIES_MAPS = {
+    "Mus musculus": "coords_mouse.json",
+    "Rattus norvegicus": "coords_rat.json",
+    "Homo sapiens": "coords_human.json",
+}
