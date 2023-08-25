cab32ee3b5d87dda5b515b7bdbaa7bdb48633757
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2023-08-25 14:28:08 +0200

    globals - Add docstrings

--- sckan_compare/globals.py
+++ sckan_compare/globals.py
@@ -1,7 +1,16 @@
+"""
+Configuration and constants for SckanCompare package.
+
+License: Apache License 2.0
+"""
+
+# Blazegraph endpoint URL
 BLAZEGRAPH_ENDPOINT = "https://blazegraph.scicrunch.io/blazegraph/sparql"
 
+# Default maximum number of days to keep cached data
 DEFAULT_MAX_CACHE_DAYS = 7
 
+# Mapping of species to their respective JSON coordinate maps
 AVAILABLE_SPECIES_MAPS = {
     "Mus musculus": "coords_mouse.json",
     "Rattus norvegicus": "coords_rat.json",
