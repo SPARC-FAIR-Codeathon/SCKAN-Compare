8f876324f62ae56c226b65285963ad9f26fc57de
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2023-08-25 14:28:19 +0200

    utils - Add docstrings

--- sckan_compare/utils.py
+++ sckan_compare/utils.py
@@ -1,6 +1,26 @@
+"""
+Utility methods for SckanCompare package.
+
+License: Apache License 2.0
+"""
+
 import pandas as pd
 
+
 def get_dataframe(data_as_list):
+    """
+    Convert a list of data to a pandas DataFrame.
+
+    Parameters
+    ----------
+    data_as_list : list
+        List of data to be converted.
+
+    Returns
+    -------
+    pandas.DataFrame
+        The converted DataFrame.
+    """
     # convert data_as_list to pandas dataframe
     df = pd.DataFrame(data_as_list)
     #set column names equal to values in row index position 0
@@ -11,6 +31,19 @@ def get_dataframe(data_as_list):
 
 
 def remove_duplicate_species(df):
+    """
+    Replace various species synonyms with standard species names in a DataFrame.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The DataFrame containing species information.
+
+    Returns
+    -------
+    pandas.DataFrame
+        The DataFrame with replaced species synonyms.
+    """
     df = df.replace('Mammalia', 'Mammal')
     df = df.replace('mammals', 'Mammal')
     df = df.replace('Vertebrata <vertebrates>', 'Vertebrata')
@@ -27,7 +60,17 @@ def remove_duplicate_species(df):
 
 def get_species_name(projection_fibre=False):
     """
-    :return: gets the (unique) name of all species in the dataset
+    Get a list of unique species names.
+
+    Parameters
+    ----------
+    projection_fibre : bool, optional
+        Whether to include projection fibre species. Defaults to False.
+
+    Returns
+    -------
+    list
+        List of unique species names.
     """
     species = ['Canis familiaris', 'Cavia porcellus', 'Homo sapiens', 'Mammal',
          'Mus musculus', 'Rattus norvegicus']
@@ -41,7 +84,12 @@ def get_species_name(projection_fibre=False):
 
 def get_phenotype_neuron():
     """
-    :return: gets unique phenotypes of neurons
+    Get a list of unique neuron phenotypes.
+
+    Returns
+    -------
+    list
+        List of unique neuron phenotypes.
     """
     phenotype = ['Enteric phenotype', 'Parasympathetic Post-Ganglionic phenotype',
           'Parasympathetic Pre-Ganglionic phenotype',
@@ -53,6 +101,14 @@ def get_phenotype_neuron():
     return phenotype
 
 def get_circuit_roles():
+    """
+    Get species and circuit roles.
+
+    Returns
+    -------
+    tuple
+        A tuple containing a list of species and a list of circuit roles.
+    """
     species = ['Homo sapiens', 'Mammal', 'Rattus norvegicus']
     circuit_roles = ['Motor phenotype', 'Sensory phenotype']
     return species, circuit_roles
