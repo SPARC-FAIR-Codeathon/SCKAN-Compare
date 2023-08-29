bbac68a8f059e8fcd4256f6270a5ce3280eb9c31
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2023-08-29 19:23:02 +0200

    Move visualization related components out

--- sckan_compare/__init__.py
+++ sckan_compare/__init__.py
@@ -16,6 +16,7 @@ from . import query
 from . import utils
 from .cachemanager import CacheManager
 from .anatomyvis import AntomyVis
+from .blockvis import BlockVis
 
 
 class SckanCompare(object):
@@ -35,7 +36,6 @@ class SckanCompare(object):
             Maximum number of days to keep cached data. Defaults to globals.DEFAULT_MAX_CACHE_DAYS (7 days).
         """
         self.endpoint = endpoint
-        self.anatomy_map_dict = {}
 
         self.cache_manager = CacheManager(os.path.join(
             os.path.dirname(__file__), 'api_cache'), max_cache_days)
@@ -264,73 +264,7 @@ class SckanCompare(object):
 
         return df_result
 
-    def load_json_species_map(self, species=None):
-        """
-        Load a JSON species map for visualization.
-
-        Parameters
-        ----------
-        species : str, optional
-            The species for which to load the map.
-
-        Raises
-        ------
-        ValueError
-            If an invalid species is specified.
-        """
-        if not species:
-            raise ValueError("species needs to be specified!")
-        if species not in self.valid_species_list:
-            raise ValueError("Invalid species specified!")
-        if species not in globals.AVAILABLE_SPECIES_MAPS.keys():
-            raise ValueError("{} visual map not currently available!".format(species))
-        
-        datapath = pkg_resources.resource_filename("sckan_compare", "data")
-        filepath = os.path.join(datapath, globals.AVAILABLE_SPECIES_MAPS[species])
-
-        with open(filepath, encoding='utf-8-sig') as json_file:
-            data = json.load(json_file)
-
-        self.anatomy_map_dict[species] = {}
-        for item in data:
-            self.anatomy_map_dict[species][item["Name"]] = [
-                int(item["X"]), int(item["Y"])]
-
-    def add_connection(self, vis_obj, region_A=None, region_B=None, region_C=None, neuron=None):
-        """
-        Add a connection to a visualization object.
-
-        Parameters
-        ----------
-        vis_obj : AntomyVis
-            The visualization object.
-        region_A : str, optional
-            The source region of the connection.
-        region_B : str, optional
-            The target region of the connection.
-        region_C : str, optional
-            An intermediate region for connections.
-        neuron : str, optional
-            The associated neuron.
-
-        Raises
-        ------
-        ValueError
-            If required parameters are missing.
-        """
-        if not region_A:
-            raise ValueError("region_A needs to be specified!")
-        if not region_B:
-            raise ValueError("region_B needs to be specified!")
-
-        if region_C:
-            # A->C->B
-            vis_obj.draw_edge_ABC(region_A, region_B, region_C, neuron)
-        else:
-            # A->B
-            vis_obj.draw_edge_AB(region_A, region_B, neuron)
-
-    def plot_dataframe_connectivity(self, df, species=None, region_A=None, region_B=None, region_C=None):
+    def plot_dataframe_anatomy_vis(self, df, species=None, region_A=None, region_B=None, region_C=None):
         """
         Plot anatomical connectivity map based on a DataFrame.
 
@@ -352,23 +286,38 @@ class SckanCompare(object):
         AntomyVis
             The visualization object.
         """
-        # load the species specific visual map
-        self.load_json_species_map(species)
-
         # create AntomyVis object
-        vis = AntomyVis(self.anatomy_map_dict[species], species)
+        vis = AntomyVis(species)
         
-        # add all connections specified in dataframe
-        for idx in range(df.shape[0]):
-            if 'Region_C' in df.columns:
-                self.add_connection(vis,
-                                    region_A=df.iloc[idx,3],
-                                    region_B=df.iloc[idx,5],
-                                    region_C=df.iloc[idx,7],
-                                    neuron=df.iloc[idx,1])
-            else:
-                self.add_connection(vis,
-                                    region_A=df.iloc[idx,3],
-                                    region_B=df.iloc[idx,5],
-                                    neuron=df.iloc[idx,1])
+        # plot all connections in dataframe
+        vis.plot_dataframe(df)
+
+        return vis
+    
+    def plot_dataframe_block_vis(self, df, region_A=None, region_B=None):
+        """
+        Plot anatomical connectivity map based on a DataFrame.
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            The DataFrame containing connectivity information.
+        species : str, optional
+            The species for visualization.
+        region_A : str, optional
+            The source region for filtering.
+        region_B : str, optional
+            The target region for filtering.
+
+        Returns
+        -------
+        AntomyVis
+            The visualization object.
+        """
+        # create AntomyVis object
+        vis = BlockVis()
+
+        # plot all connections in dataframe
+        vis.plot_figure(df, region_A, region_B)
+
         return vis
